0cb4b8880fb8bf3a39ad31e3bb79d966d849b22b
napakalas <yuda.munarko@gmail.com>
2024-10-22 13:53:50 +1300

    update search example

--- scripts/search.py
+++ scripts/search.py
@@ -1,8 +1,8 @@
-from anatomy_lookup import AnatomyLookup
+from anatomy_lookup import AnatomyLookup, get_uriref, get_uri
 
 if __name__ == '__main__':
     lookup = AnatomyLookup()
-
     print(lookup.search('Wall of larynx'))
-
+    print(lookup.get_ancestor('UBERON:0001869'))
+    print(lookup.search_candidates('prostate epithelium', uri_candidates=['UBERON:0000002', 'UBERON:0000079'], k=10000, force=True))
     lookup.close()
