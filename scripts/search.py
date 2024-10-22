604226b53f841096acb97dcc6c7fc1464393a3b6
napakalas <yuda.munarko@gmail.com>
2024-10-23 12:36:58 +1300

    update search example

--- scripts/search.py
+++ scripts/search.py
@@ -1,8 +1,21 @@
 from anatomy_lookup import AnatomyLookup
+from pprint import pp, pprint
 
 if __name__ == '__main__':
     lookup = AnatomyLookup()
-    print(lookup.search('Wall of larynx'))
-    print(lookup.get_ancestor('UBERON:0001869'))
-    print(lookup.search_candidates('prostate epithelium', uri_candidates=['UBERON:0000002', 'UBERON:0000079'], k=10000, force=True))
+    print("search('Wall of larynx'):")
+    pprint(lookup.search('Wall of larynx'))
+    print()
+
+    print("lookup.get_ancestor('UBERON:0001869'):")
+    pprint(lookup.get_ancestor('UBERON:0001869'))
+    print()
+
+    print("search_candidates('prostate epithelium', uri_candidates=['UBERON:0000002', 'UBERON:0000079'], force=True):")
+    pprint(lookup.search_candidates('prostate epithelium', uri_candidates=['UBERON:0000002', 'UBERON:0000079'], force=True))
+    print()
+
+    print("search_with_scope('prostate epithelium', scope=['male reproductive system']):")
+    pprint(lookup.search_with_scope('prostate epithelium', scope=['male reproductive system']))
+
     lookup.close()
